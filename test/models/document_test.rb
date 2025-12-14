68c3a42921f0b4b052e669a88f42df1109cbcd59
sato-git-hub <mimilili.disney@gmail.com>
2025-12-14 13:21:18 +0900

    テストファイルの修正

--- test/models/document_test.rb
+++ test/models/document_test.rb
@@ -1,5 +1,10 @@
 require "test_helper"
 
 class DocumentTest < ActiveSupport::TestCase
+  test "title should be present" do
+    user = User.create!(email: "a@test.com", password: "password")
+    document = user.documents.build(title: "")
 
+    assert_not document.valid?
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,7 +4,7 @@ class UserTest < ActiveSupport::TestCase
   # やりたいこと：passwordカラムを5文字以下で入力させない
   test "password should be too short" do
     user = User.new(email: "unique_email@example.com", password: "aaa")
-    # user.valid?  が true であることを検証する
+    # user.valid?  が false であることを検証する
     assert_not user.valid? 
   end
 end
