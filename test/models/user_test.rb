3cee0e9103e9c34cf0f6549c826756555b022797
sato-git-hub <mimilili.disney@gmail.com>
2025-12-22 23:09:03 +0900

    コメントの削除

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,10 +1,8 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # やりたいこと：passwordカラムを5文字以下で入力させない
   test "password should be too short" do
     user = User.new(email: "unique_email@example.com", password: "aaa")
-    # user.valid?  が false であることを検証する
     assert_not user.valid? 
   end
 end
